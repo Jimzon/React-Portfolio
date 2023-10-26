f6c9be993fdf680115a75c68bf60afce2d867a3f
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-26 20:30:04 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/NavBar/navbar";
+import Intro from "./components/Intro/intro";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
