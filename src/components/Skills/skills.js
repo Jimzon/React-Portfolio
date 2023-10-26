7bb020f3cb6f67662c3e7a475687785b46186e3c
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-27 00:19:24 +0800

    modified skills js

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./skills.css";
+import UIDesign from "../../assets/ui-design.png";
+import WebDesign from "../../assets/website-design.png";
+import AppDesign from "../../assets/app-design.png";
 
 const Skills = () => {
-  return <section id="skills">About section</section>;
+  return (
+    <section id="skills">
+      <span className="skillTitle">What I Do</span>
+      <span className="skillDesc">
+        I specialize in web design, crafting beautiful and user-friendly
+        websites, while also diving into programming to bring them to life
+      </span>
+    </section>
+  );
 };
 
 export default Skills;
