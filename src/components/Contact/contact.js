84551c6b19cd33e7020ea689b927aaf88bddc07c
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-27 14:05:59 +0800

    imported clients imgs

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "./contact.css";
+import Walmart from "../../assets/walmart.png";
+import Adobe from "../../assets/adobe.png";
+import Microsoft from "../../assets/microsoft.png";
+import Facebook from "../../assets/facebook.png";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <section className="contactPage">
+      <div id="clients">
+        <h1 className="contactPageTitle">My Clients</h1>
+        <p className="clientDesc">
+          I have served some of the best companies. Some of the notable
+          companies I have worked with includes
+        </p>
+        <div className="clientImgs">
+          <img className="clientImg" src={Walmart} alt="" />
+          <img className="clientImg" src={Adobe} alt="" />
+          <img className="clientImg" src={Microsoft} alt="" />
+          <img className="clientImg" src={Facebook} alt="" />
+        </div>
+      </div>
+      <div id="contact"></div>
+    </section>
+  );
 };
 
 export default Contact;
